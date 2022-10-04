91f2bd4d3d3aebc2b5bf06556465ff57c45927a3
kakawanyifan <registermail@qq.com>
2022-10-04 09:22:12 +0800

    加了一些注释

--- agent.py
+++ agent.py
@@ -1,58 +1,70 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import argparse
+import logging
+import os
+import time
+from collections import deque
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.tensorboard import SummaryWriter
-import gym
-import random
-from collections import deque, namedtuple
-import logging
-from tqdm import tqdm
-import argparse
-import os
-import time
 import yaml
-import csv
-from copy import deepcopy
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
 from optionpricing import *
 
 # Defining transition namedtuple here rather than within the class to ensure pickle functionality
 transition = namedtuple('transition',
-            ['old_state', 'action', 'reward', 'new_state', 'done'])
+                        ['old_state', 'action', 'reward', 'new_state', 'done'])
 
 
 class Estimator(nn.Module):
     def __init__(self, nhidden, nunits, state_space_dim, action_space_dim):
         """
-        Estimator class that returns Q-values
-            ngpu: number of gpus
-            state_space_dim: Dimension of the state space
-            action_space_dim: Dimension of the action space
+        DQN-Agent内部的神经网络
+        :param nhidden: 隐藏层网络数
+        :param nunits: 每一层的节点数
+        :param state_space_dim: 状态空间的维度
+        :param action_space_dim: 动作空间的维度
         """
 
         super(Estimator, self).__init__()
         self.state_space_dim = state_space_dim
         self.action_space_dim = action_space_dim
 
+        # 断言
         assert nhidden > 0, 'Number of hidden layers must be > 0'
 
+        # 初始化神经网络
+        # 第一层
         init_layer = nn.Linear(state_space_dim, nunits)
+        # 最后一层
         self.final_layer = nn.Linear(nunits, action_space_dim)
 
+        # 其他层
         layers = [init_layer]
         for n in range(nhidden - 1):
             layers.append(nn.Linear(nunits, nunits))
 
         self.module_list = nn.ModuleList(layers)
 
+        # 激活函数
         self.relu = nn.ReLU()
 
     def forward(self, x):
+        """
+        前向计算
+        :param x:
+        :return:
+        """
+        # 遍历每一层
         for module in self.module_list:
+            # 神经网络
             x = module(x)
+            # 激活函数
             x = self.relu(x)
 
+        # 最后一层
         x = self.final_layer(x)
 
         return x
@@ -61,13 +73,14 @@ class Estimator(nn.Module):
 class Agent:
     def __init__(self, env, args):
         """
-        Agent class to train the DQN
-            env: Gym like environment object
-            args: Training arguments | use --help flag to view
+        智能体
+        :param env: 环境
+        :param args: 参数
         """
         self.env = env
         self.args = args
 
+        # epsilon 贪心
         self.epsilon = args.epsilon
         self.decay = args.decay
         self.gamma = args.gamma
@@ -75,15 +88,19 @@ class Agent:
         self.replay_memory_size = args.replay_memory_size
         self.update_every = args.update_every
         self.epsilon_min = args.epsilon_min
+        # 保存目录
         self.savedir = args.savedir
         self.scale = args.scale
         if args.clip == 0:
             self.clip = np.inf
         else:
             self.clip = args.clip
-        self.best_reward_criteria = args.best_reward_criteria # If mean reward over last 'best_reward_critera' > best_reward, save model
+
+        # If mean reward over last 'best_reward_critera' > best_reward, save model
+        self.best_reward_criteria = args.best_reward_criteria
 
         # Get valid actions
+        # 获取有效动作
         try:
             self.valid_actions = list(range(env.action_space.n))
         except AttributeError as e:
@@ -92,26 +109,28 @@ class Agent:
         # Logging
         self.train_logger = logging.getLogger('train')
         self.train_logger.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s, %(message)s', datefmt = '%Y-%m-%d %H:%M:%S')
+        formatter = logging.Formatter('%(asctime)s, %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
         file_handler = logging.FileHandler(os.path.join('experiments', args.savedir, 'training.log'))
         file_handler.setFormatter(formatter)
         self.train_logger.addHandler(file_handler)
         self.train_logger.propagate = False
 
         # Tensorboard
-        self.writer = SummaryWriter(log_dir = os.path.join('experiments', self.savedir), flush_secs = 5)
+        self.writer = SummaryWriter(log_dir=os.path.join('experiments', self.savedir), flush_secs=5)
 
         # Initialize model
         self.device = torch.device("cuda:0" if args.cuda else "cpu")
         state_shape = env.observation_space.shape
         state_space_dim = state_shape[0] if len(state_shape) == 1 else state_shape
 
+        # 估计器
         self.estimator = Estimator(args.nhidden, args.nunits, state_space_dim, env.action_space.n).to(self.device)
+        # 目标网络
         self.target = Estimator(args.nhidden, args.nunits, state_space_dim, env.action_space.n).to(self.device)
 
         # Optimization
-        self.criterion = nn.SmoothL1Loss(reduction = 'mean')
-        self.optimizer = optim.Adam(self.estimator.parameters(), lr = args.lr, betas = (args.beta1, 0.999))
+        self.criterion = nn.SmoothL1Loss(reduction='mean')
+        self.optimizer = optim.Adam(self.estimator.parameters(), lr=args.lr, betas=(args.beta1, 0.999))
 
         # If resume, load from checkpoint | otherwise initialize
         if args.resume:
@@ -122,7 +141,8 @@ class Agent:
                 print('Checkpoint not found')
 
         else:
-            self.replay_memory = deque(maxlen = args.replay_memory_size)
+            # 经验回放队列
+            self.replay_memory = deque(maxlen=args.replay_memory_size)
 
             # Initialize replay memory
             self.initialize_replay_memory(self.batch_size)
@@ -134,7 +154,6 @@ class Agent:
             self.episode = 0
             self.steps = 0
             self.best_reward = -self.clip * self.env.T * self.env.D
-
 
     def initialize_replay_memory(self, size):
         """
@@ -153,7 +172,7 @@ class Agent:
             new_state, reward, done, _ = self.env.step(action)
             reward = np.clip(self.scale * reward, -self.clip, self.clip)
             self.replay_memory.append(transition(old_state, action,
-                reward, new_state, done))
+                                                 reward, new_state, done))
 
             if done:
                 old_state = self.env.reset()
@@ -162,10 +181,12 @@ class Agent:
 
         self.train_logger.info(f'INFO: Replay memory initialized with {size} experiences')
 
-
     def train(self, nepisodes, episode_length):
         """
-        Train the agent
+        训练智能体
+        :param nepisodes:
+        :param episode_length:
+        :return:
         """
         train_rewards = []
 
@@ -177,7 +198,8 @@ class Agent:
             episode_history = []
             losses = []
             done = False
-            kind = None # Type of action taken
+            # Type of action taken
+            kind = None
 
             old_state = self.env.reset()
 
@@ -186,39 +208,35 @@ class Agent:
                 stock_price = self.env.S
                 call = self.env.call
 
-                ####################################################
-                # Select e-greedy action                           #
-                ####################################################
+                # 选择 e-greedy 动作
                 if random.random() <= self.epsilon:
+                    # 随机选择一个动作
                     action = random.choice(self.valid_actions)
+                    # 随机的
                     kind = 'random'
-
                 else:
                     with torch.no_grad():
                         old_state = torch.from_numpy(old_state.reshape(1, -1)).to(self.device)
                         action = np.argmax(self.estimator(old_state).cpu().numpy())
                         old_state = old_state.cpu().numpy().reshape(-1)
+                    # 根据策略的
                     kind = 'policy'
 
-                ####################################################
-                # Env step and store experience in replay memory   #
-                ####################################################
+                # 新的状态
                 new_state, reward, done, info = self.env.step(action)
+                # 奖励
                 reward = np.clip(self.scale * reward, -self.clip, self.clip)
 
-                self.replay_memory.append(transition(old_state, action,
-                    reward, new_state, done))
+                # 存储至经验回放
+                self.replay_memory.append(transition(old_state, action, reward, new_state, done))
 
-                episode_history.append(transition(old_state, action,
-                    reward, new_state, done))
+                episode_history.append(transition(old_state, action, reward, new_state, done))
 
                 episode_rewards.append(reward)
                 episode_steps += 1
                 self.steps += 1
 
-                ####################################################
-                # Sample batch and fit to model                    #
-                ####################################################
+                # 抽样
                 batch = random.sample(self.replay_memory, self.batch_size)
                 old_states, actions, rewards, new_states, is_done = map(np.array, zip(*batch))
                 rewards = rewards.astype(np.float32)
@@ -231,7 +249,7 @@ class Agent:
 
                 with torch.no_grad():
                     q_target = self.target(new_states)
-                    max_q, _ = torch.max(q_target, dim = 1)
+                    max_q, _ = torch.max(q_target, dim=1)
                     q_target = rewards + self.gamma * is_not_done.float() * max_q
 
                 # Gather those Q values for which action was taken | since the output is Q values for all possible actions
@@ -254,7 +272,8 @@ class Agent:
                 self.writer.add_scalar('Transition/loss', loss, self.steps)
 
                 # Log statistics
-                self.train_logger.info(f'LOG: episode:{self.episode}, step:{episode_steps}, action:{action}, kind:{kind}, reward:{reward}, best_mean_reward:{self.best_reward}, loss:{losses[-1]}, epsilon:{self.epsilon}, S:{stock_price}, c:{call}, delta:{delta}, n:{self.env.n}, dn:{info["dn"]}, cost:{info["cost"]}, pnl:{info["pnl"]}, K:{self.env.K}, T:{self.env.T}')
+                self.train_logger.info(
+                    f'LOG: episode:{self.episode}, step:{episode_steps}, action:{action}, kind:{kind}, reward:{reward}, best_mean_reward:{self.best_reward}, loss:{losses[-1]}, epsilon:{self.epsilon}, S:{stock_price}, c:{call}, delta:{delta}, n:{self.env.n}, dn:{info["dn"]}, cost:{info["cost"]}, pnl:{info["pnl"]}, K:{self.env.K}, T:{self.env.T}')
 
                 if episode_steps >= episode_length:
                     break
@@ -278,11 +297,11 @@ class Agent:
             if not self.episode % self.args.checkpoint_every:
                 self.save_checkpoint(os.path.join('experiments', self.args.savedir, 'checkpoint.pt'))
 
-
     def save_checkpoint(self, path):
         """
-        Checkpoint the model
-            path: Save path
+        保存模型
+        :param path: 文件路径
+        :return:
         """
         checkpoint = {
             'episode': self.episode,
@@ -301,8 +320,9 @@ class Agent:
 
     def load_checkpoint(self, path):
         """
-        Load checkpoint
-            path: Checkpoint (checkpoint.pt) path
+        加载模型
+        :param path: 文件路径
+        :return:
         """
         checkpoint = torch.load(path)
         self.episode = checkpoint['episode']
@@ -320,35 +340,50 @@ class Agent:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--nepisodes', type = int, default = 15000, help = 'number of episodes to train')
-    parser.add_argument('--episode_length', type = int, default = 1000, help = 'maximum episode length')
-    parser.add_argument('--epsilon', type = float, default = 1, help = 'starting e-greedy probability')
-    parser.add_argument('--decay', type = float, default = 0.999, help = 'decay of epsilon per episode')
-    parser.add_argument('--epsilon_min', type = float, default = 0.005, help = 'minumum value taken by epsilon')
-    parser.add_argument('--gamma', type = float, default = 0.3, help = 'discount factor')
-    parser.add_argument('--update_every', type = int, default = 500, help = 'update target model every [_] steps')
-    parser.add_argument('--checkpoint_every', type = int, default = 1000, help = 'checkpoint model every [_] steps')
-    parser.add_argument('--resume', action = 'store_true', help = 'resume from previous checkpoint from save directory')
-    parser.add_argument('--batch_size', type = int, default = 128, help = 'batch size')
-    parser.add_argument('--replay_memory_size', type = int, default = 64000, help = 'replay memory size')
-    parser.add_argument('--seed', type = int, help = 'random seed')
-    parser.add_argument('--savedir', type = str, help = 'save directory')
-    parser.add_argument('--nhidden', type = int, default = 2, help = 'number of hidden layers')
-    parser.add_argument('--nunits', type = int, default = 128, help = 'number of units in a hidden layer')
-    parser.add_argument('--lr', type = float, default = 0.001, help = 'learning rate')
-    parser.add_argument('--beta1', type = float, default = 0.9, help = 'beta1')
-    parser.add_argument('--cuda', action = 'store_true', help = 'cuda')
-    parser.add_argument('--scale', type = float, default = 1, help = 'scale reward by [_] | reward = [_] * reward | Takes priority over clip')
-    parser.add_argument('--clip', type = float, default = 100, help = 'clip reward between [-clip, clip] | Pass in 0 for no clipping')
-    parser.add_argument('--best_reward_criteria', type = int, default = 10, help = 'save model if mean reward over last [_] episodes greater than best reward')
-    parser.add_argument('--trc_multiplier', type = float, default = 1, help = 'transaction cost multiplier')
-    parser.add_argument('--trc_ticksize', type = float, default = 0.1, help = 'transaction cost ticksize')
+    parser.add_argument('--nepisodes', type=int, default=15000, help='number of episodes to train')
+    parser.add_argument('--episode_length', type=int, default=1000, help='maximum episode length')
+    parser.add_argument('--epsilon', type=float, default=1, help='starting e-greedy probability')
+    # 每一个 episode 中 epsilon 的衰减
+    parser.add_argument('--decay', type=float, default=0.999, help='decay of epsilon per episode')
+    parser.add_argument('--epsilon_min', type=float, default=0.005, help='minumum value taken by epsilon')
+    # 折扣因子
+    parser.add_argument('--gamma', type=float, default=0.3, help='discount factor')
+    # 每隔多久更新一下 target model
+    parser.add_argument('--update_every', type=int, default=500, help='update target model every [_] steps')
+    # 每隔多久保存一下模型
+    parser.add_argument('--checkpoint_every', type=int, default=1000, help='checkpoint model every [_] steps')
+    parser.add_argument('--resume', action='store_true', help='resume from previous checkpoint from save directory')
+    # 经验回放的样本大小
+    parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+    # 经验回放的大小
+    parser.add_argument('--replay_memory_size', type=int, default=64000, help='replay memory size')
+    # 随机数种子
+    parser.add_argument('--seed', type=int, help='random seed')
+    # 保存目录
+    parser.add_argument('--savedir', type=str, help='save directory')
+    # 隐藏层网络的层数
+    parser.add_argument('--nhidden', type=int, default=2, help='number of hidden layers')
+    # 隐藏层网络的节点数
+    parser.add_argument('--nunits', type=int, default=128, help='number of units in a hidden layer')
+    # 学习率
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+    parser.add_argument('--beta1', type=float, default=0.9, help='beta1')
+    parser.add_argument('--cuda', action='store_true', help='cuda')
+    parser.add_argument('--scale', type=float, default=1,  help='scale reward by [_] | reward = [_] * reward | Takes priority over clip')
+    # 克服梯度弥散和梯度爆炸
+    parser.add_argument('--clip', type=float, default=100,  help='clip reward between [-clip, clip] | Pass in 0 for no clipping')
+    parser.add_argument('--best_reward_criteria', type=int, default=10,
+                        help='save model if mean reward over last [_] episodes greater than best reward')
+    parser.add_argument('--trc_multiplier', type=float, default=1, help='transaction cost multiplier')
+    parser.add_argument('--trc_ticksize', type=float, default=0.1, help='transaction cost ticksize')
 
     args = parser.parse_args()
 
+    # 随机数种子
     if args.seed is None:
         args.seed = random.randint(1, 10000)
 
+    # 保存目录
     if args.savedir is None:
         args.savedir = time.strftime('%Y-%m-%d_%H:%M:%S', time.localtime())
 
@@ -367,9 +402,9 @@ if __name__ == '__main__':
 
     config = {
         'S': 100,
-        'T': 10, # 10 days
+        'T': 10,  # 10 days
         'L': 1,
-        'm': 100, # L options for m stocks
+        'm': 100,  # L options for m stocks
         'n': 0,
         'K': [95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105],
         'D': 5,
@@ -380,7 +415,7 @@ if __name__ == '__main__':
         'kappa': 0.1,
         'multiplier': args.trc_multiplier,
         'ticksize': args.trc_ticksize
-        }
+    }
     env = OptionPricingEnv(config)
     env.configure()
 
--- optionpricing.py
+++ optionpricing.py
@@ -1,46 +1,86 @@
-import gym
-from gym import error, spaces, utils
-import numpy as np
 import random
-from scipy.stats import norm
 from collections import namedtuple
 
+import numpy as np
+from gym import spaces
+from scipy.stats import norm
+
+
 def compute_call(S, K, t, r, sigma):
+    """
+    基于BSM计算看涨期权的价格
+    :param S: 标的资产价格
+    :param K: 行权价
+    :param t: 期限
+    :param r: 无风险利率
+    :param sigma: 波动率
+    :return: 看涨期权的价格
+    """
     if np.isclose(t, 0):
         return max(0, S - K)
 
     if t == 0:
         return max(0, S - K)
 
-    d1 = ((np.log(S / K) + (r + sigma ** 2 / 2) * t)) / sigma / np.sqrt(t)
+    d1 = (np.log(S / K) + (r + sigma ** 2 / 2) * t) / sigma / np.sqrt(t)
     d2 = d1 - sigma * np.sqrt(t)
+    # norm.cdf：标准正态分布的累计概率分布函数
     call = S * norm.cdf(d1) - K * np.exp(-r * t) * norm.cdf(d2)
 
     return call
 
+
 def compute_greeks(S, K, t, r, sigma):
+    """
+    计算希腊字母，风险
+    :param S: 标的资产
+    :param K: 行权价
+    :param t: 期限
+    :param r: 无风险利率
+    :param sigma: 波动率
+    :return: delta(期权价格关于标的资产价格的敏感度) gamma(Delta关于标的资产价格的微小变化的敏感性)
+    """
     if np.isclose(t, 0):
         return 1, 0
 
     if t == 0:
         return 1, 0
 
-    d1 = ((np.log(S / K) + (r + sigma ** 2 / 2) * t)) / sigma / np.sqrt(t)
+    d1 = (np.log(S / K) + (r + sigma ** 2 / 2) * t) / sigma / np.sqrt(t)
     delta = norm.cdf(d1)
     gamma = (1 / np.sqrt(2 * np.pi)) * np.exp(-d1 ** 2 / 2) / (S * sigma * np.sqrt(t))
 
     return delta, gamma
 
+
 def compute_pnl(init_portfolio, final_portfolio):
+    """
+    计算成本
+    :param init_portfolio: 初始时刻的投资组合
+    :param final_portfolio: 结束时刻的投资组合
+    :return: 成本
+    """
+    # 初始时刻投资组合的价值
     init_wealth = compute_wealth(init_portfolio)
+    # 结束时刻投资组合的价值
     final_wealth = compute_wealth(final_portfolio)
 
+    # 成本
     return final_wealth - init_wealth
 
 
 def compute_wealth(portfolio):
+    """
+    计算价值
+    :param portfolio: 投资组合
+    :return: 价值
+    """
+    # 期权价值
+    # TODO
     option_value = portfolio.call * portfolio.m * portfolio.L
+    # 标的资产价值 = 每一个标的资产的价值 * 数量
     stock_value = portfolio.S * portfolio.n
+    # 现金
     cash = portfolio.cash
 
     return option_value + stock_value + cash
@@ -49,46 +89,63 @@ def compute_wealth(portfolio):
 class OptionPricingEnv:
     def __init__(self, config):
         """
+        期权定价环境
+        :param config:
         config: Configuration dictionary with k:v as
-            S: stock price (float)
-            T: days to maturity (int or list of ints)
-            L: number of option contracts (int)
-            m: number of stocks per option (int)
-            n: number of stocks (int)
-            K: strike price (float or list of floats)
+            S: stock price (float)      标的资产价格
+            T: days to maturity (int or list of ints)      到期时间
+            L: number of option contracts (int)      期权合约数
+            m: number of stocks per option (int)      每个期权的标的资产数
+            n: number of stocks (int)      标的资产数
+            K: strike price (float or list of floats)      行权价
+            # TODO
             D: trading periods per day (int)
-            mu: expected rate of return on the stock (float)
-            sigma: volatility of stock (float)
-            r: risk free rate (float)
+            mu: expected rate of return on the stock (float)      标的资产的预期回报
+            sigma: volatility of stock (float)      标的资产的波动率
+            r: risk free rate (float)      无风险利率
+            # TODO
             ss: number of steps between trading periods (int)
+            # 风险厌恶
             kappa: risk aversion (float)
         """
         self.config = config
 
         self.trading_days = 252
-        self.day = 24 / self.trading_days # 24 hours
+        # 24 hours
+        self.day = 24 / self.trading_days
         self.lots = 1
 
         self.configured = False
 
     @property
     def call(self):
+        # 看涨期权的价格
         return compute_call(self.S, self.K, self.t, self.r, self.sigma)
 
     @property
     def portfolio(self):
+        # 投资组合
+        # s: 标的资产
+        # call: 看涨期权
+        # n: 标的资产数
+        # m: 每个期权的标的资产数
+        # L: 期权合约数
+        # cash: 现金
         return namedtuple('Portfolio', ['S', 'call', 'n', 'm', 'L', 'cash'])(self.S, self.call, self.n, self.m, self.L, self.cash)
 
     @property
     def stock_value(self):
+        # 标的资产价值：每一个标的资产的价值 * 数量
         return self.n * self.S
 
     @property
     def option_value(self):
+        # TODO
         return self.call * self.m * self.L
 
     @property
     def delta(self):
+        # Delte(期权价格关于标的资产价格的敏感度)
         delta, gamma = compute_greeks(self.S, self.K, self.t, self.r, self.sigma)
         return delta
 
@@ -107,10 +164,10 @@ class OptionPricingEnv:
         except TypeError:
             self.K = self.config['K']
 
-        #self.K = K
+        # self.K = K
         self.D = self.config['D']
         self.mu = self.config['mu']
-        self.sigma = self.config['sigma'] * np.sqrt(self.trading_days) # Converting sigma/day to sigma/year
+        self.sigma = self.config['sigma'] * np.sqrt(self.trading_days)  # Converting sigma/day to sigma/year
         self.r = self.config['r']
         self.ss = self.config['ss']
         self.kappa = self.config['kappa']
@@ -120,7 +177,7 @@ class OptionPricingEnv:
         self.S0 = self.S
         self.cash = 0
 
-        #self.init_config = {k: v for k, v in locals().items() if k != 'self'}
+        # self.init_config = {k: v for k, v in locals().items() if k != 'self'}
 
         self.t = self.day * self.T
         self.steps = self.T * self.D
@@ -135,7 +192,8 @@ class OptionPricingEnv:
 
         self.high = h
 
-        self.observation_space = spaces.Box(low = np.array([0, 0, 0, -np.inf]), high = np.array([np.inf, np.inf, np.inf, np.inf]))
+        self.observation_space = spaces.Box(low=np.array([0, 0, 0, -np.inf]),
+                                            high=np.array([np.inf, np.inf, np.inf, np.inf]))
         self.action_space = spaces.Discrete(num_actions)
 
         self.action_map = {i: int(l + i * self.lots) for i in range(self.action_space.n)}
@@ -144,7 +202,7 @@ class OptionPricingEnv:
         self.configured = True
         self.done = False
 
-    def step(self, action, stock_prices = None):
+    def step(self, action, stock_prices=None):
         """
         stock_prices: for deterministic evolution | dtype: list (even if single entry)
         """
@@ -194,15 +252,16 @@ class OptionPricingEnv:
 
         reward = (pnl - 0.5 * self.kappa * (pnl ** 2))
 
-        info = {'pnl': pnl, 'dn': num_stocks, 'call': np.array(calls), 'delta': np.array(deltas), 'gamma': np.array(gammas), 'cost': cost}
+        info = {'pnl': pnl, 'dn': num_stocks, 'call': np.array(calls), 'delta': np.array(deltas),
+                'gamma': np.array(gammas), 'cost': cost}
 
         self.done = self.steps == 0
 
-        return np.array(states[-1], dtype = np.float32), reward, self.done, info
+        return np.array(states[-1], dtype=np.float32), reward, self.done, info
 
     def reset(self):
         self.configure()
-        return np.array([self.S / self.S0, self.t, self.n / self.high, self.K / self.S0], dtype = np.float32)
+        return np.array([self.S / self.S0, self.t, self.n / self.high, self.K / self.S0], dtype=np.float32)
 
     def render(self):
         pass
